15c65270ec81afd62d13bfcfadab96b10af0ca25
Laeticia Pierre <laeticia.pierre06200@gmail.com>
2018-02-09 11:00:01 +0100

    Résolution conflit combatMascottesActivity

--- app/src/main/java/mbds/barcodebattler/CombatMascottesActivity.java
+++ app/src/main/java/mbds/barcodebattler/CombatMascottesActivity.java
@@ -2,7 +2,6 @@ package mbds.barcodebattler;
 
 import android.database.DataSetObserver;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
@@ -15,12 +14,9 @@ import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-
-import java.io.ByteArrayOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Objects;
-import java.util.Random;
 
 public class CombatMascottesActivity extends AppCompatActivity implements ListAdapter {
 
@@ -46,20 +42,6 @@ public class CombatMascottesActivity extends AppCompatActivity implements ListAd
         logsCombat = new ArrayList<>();
         attributionCouleurs = new HashMap();
 
-<<<<<<< HEAD
-        mascotte1 = getIntent().getExtras().getParcelable("mascotte1");
-        Drawable img1 = getResources().getDrawable( mascotte1.getIdImage());
-        Bitmap imgBitmap1 = ((BitmapDrawable) img1).getBitmap();
-
-        mascotte2 = getIntent().getExtras().getParcelable("mascotte2");
-        Drawable img2 = getResources().getDrawable( mascotte2.getIdImage());
-        Bitmap imgBitmap2 = ((BitmapDrawable) img2).getBitmap();
-
-        // Si les deux mascottes sont du même type, on les différencie
-        if (Objects.equals(mascotte1.getNom(), mascotte2.getNom())) {
-            mascotte1.setNom(mascotte1.getNom() + " (1)");
-            mascotte2.setNom(mascotte2.getNom() + " (2)");
-=======
         Mascotte vainqueur;
 
         // Si des logs sont envoyés, on les charge
@@ -69,11 +51,7 @@ public class CombatMascottesActivity extends AppCompatActivity implements ListAd
             mascotte1 = this.logsCombat.get(0).attaquant;
             mascotte2 = this.logsCombat.get(1).attaquant;
 
-            // Si les deux mascottes sont du même type, on les différencie
-            if (Objects.equals(mascotte1.getNom(), mascotte2.getNom())) {
-                mascotte1.setNom(mascotte1.getNom() + " (1)");
-                mascotte2.setNom(mascotte2.getNom() + " (2)");
-            }
+            adapterNomsSiIdentiques();
 
             // vainqueur = dernier attaquant
             vainqueur = this.logsCombat.get(this.logsCombat.size()-1).attaquant;
@@ -81,23 +59,11 @@ public class CombatMascottesActivity extends AppCompatActivity implements ListAd
         else {
 
             mascotte1 = getIntent().getExtras().getParcelable("mascotte1");
-            Bitmap b = BitmapFactory.decodeByteArray(
-                    getIntent().getByteArrayExtra("Image1"), 0, getIntent().getByteArrayExtra("Image1").length);
-            mascotte1.setImage(b);
-
             mascotte2 = getIntent().getExtras().getParcelable("mascotte2");
-            b = BitmapFactory.decodeByteArray(
-                    getIntent().getByteArrayExtra("Image2"), 0, getIntent().getByteArrayExtra("Image2").length);
-            mascotte2.setImage(b);
 
-            // Si les deux mascottes sont du même type, on les différencie
-            if (Objects.equals(mascotte1.getNom(), mascotte2.getNom())) {
-                mascotte1.setNom(mascotte1.getNom() + " (1)");
-                mascotte2.setNom(mascotte2.getNom() + " (2)");
-            }
+            adapterNomsSiIdentiques();
 
             vainqueur = ServiceCombat.lancerCombat(mascotte1, mascotte2, this.logsCombat);
->>>>>>> 1fc84dd6e3fec5fe2fc3c9c0b29923ea85301aab
         }
 
         nbTours = this.logsCombat.size();
@@ -106,17 +72,16 @@ public class CombatMascottesActivity extends AppCompatActivity implements ListAd
         attributionCouleurs.put(mascotte2.getNom(), Color.GRAY);
 
         imageMascotte1 = (ImageView) findViewById(R.id.imagemascotte1);
-        //Bitmap i1 = BitmapFactory.decodeResource(getResources(), R.drawable.ame_des_aspects);
-        //mascotte1.setImage(i1);
-        imageMascotte1.setImageBitmap(imgBitmap1);
+        Drawable img = getResources().getDrawable( mascotte1.getIdImage());
+        Bitmap imgBitmap = ((BitmapDrawable) img).getBitmap();
+        imageMascotte1.setImageBitmap(imgBitmap);
         imageMascotte1.setBackgroundColor(Color.LTGRAY);
 
         imageMascotte2 = (ImageView) findViewById(R.id.imagemascotte2);
-        //Bitmap i2 = BitmapFactory.decodeResource(getResources(), R.drawable.albie);
-        //mascotte2.setImage(i2);
+        Drawable img2 = getResources().getDrawable( mascotte2.getIdImage());
+        Bitmap imgBitmap2 = ((BitmapDrawable) img2).getBitmap();
         imageMascotte2.setImageBitmap(imgBitmap2);
         imageMascotte2.setBackgroundColor(Color.GRAY);
-
 
         texteResultat = (TextView) findViewById(R.id.vainqueur);
         texteResultat.setText(texteResultat.getText() + vainqueur.getNom() + "");
@@ -223,5 +188,12 @@ public class CombatMascottesActivity extends AppCompatActivity implements ListAd
     @Override
     public boolean isEmpty() {
         return (logsCombat.size() == 0);
+    }
+
+    public void adapterNomsSiIdentiques() {
+        if (Objects.equals(mascotte1.getNom(), mascotte2.getNom())) {
+            mascotte1.setNom(mascotte1.getNom() + " (1)");
+            mascotte2.setNom(mascotte2.getNom() + " (2)");
+        }
     }
 }
